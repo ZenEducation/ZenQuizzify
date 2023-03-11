ccfe0b1fc887deb3172203d3669e7c66e68a3db3
Shreyash Pandey <shreyash.pandey@leadsquared.com>
2023-03-11 21:37:11 +0530

    added pdf view

---
+++ pages/pdfView/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <NuxtLayout name="zen">
+        <PdfView/>
+      </NuxtLayout>
+    </div>
+  </template>
+  
+  <script setup>
+  import PdfView from "@/components/PdfView/PdfView.vue";
+  </script>
